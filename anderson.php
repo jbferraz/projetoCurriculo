2faa9beb469f4213fce1c81817287d9a933a0762
anderson203 <>
2017-03-23 21:22:33 -0300

    curriculo anderson

---
+++ anderson.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>anderson</h1>
+        <p>
+            eu sou um boy que like play games e jogar futebol, caso i lose eu
+            fico pistola
+        </p>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
