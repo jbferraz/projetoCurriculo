02ee318f2266505feff2fd952ed6581798939e3c
jbferraz <>
2017-03-23 21:03:13 -0300

    meu 1º envio

---
+++ jair.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Jair</h1>
+        <p>
+            Mussum Ipsum, cacilds vidis litro abertis. Praesent vel viverra nisi. 
+            Mauris aliquet nunc non turpis scelerisque, eget. 
+            Manduma pindureta quium dia nois paga. 
+            Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo! 
+            Viva Forevis aptent taciti sociosqu ad litora torquent
+        </p>
+        <p>
+            <a href="index.php">Voltar</a>
+        </p>
+        
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
