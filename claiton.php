2ef7b660c41d9cb0cd144d9641347e752270c6ce
aluno <aluno@192.168.200.191>
2017-03-23 21:22:06 -0300

    Luzardo para de ser atucanado!!!

---
+++ claiton.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Claiton</h1>
+        <p>
+            Não entendi nada saporra. Isto é coisa pra louco!
+        </p>
+        <p>
+            <a href="index.php">Voltar</a>
+        </p>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
